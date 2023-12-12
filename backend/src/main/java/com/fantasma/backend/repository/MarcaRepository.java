432e38abeab0f212cd356b1550bfa445dcdc715a
phg <paulo.grande@matera.com>
2023-12-12 10:26:34 -0300

    Crud de Marca

---
+++ backend/src/main/java/com/fantasma/backend/repository/MarcaRepository.java
@@ -0,0 +1,9 @@
+package com.fantasma.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fantasma.backend.entity.Marca;
+
+public interface MarcaRepository extends JpaRepository<Marca, Long>{
+
+}
