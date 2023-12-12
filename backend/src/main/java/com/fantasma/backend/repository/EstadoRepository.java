6213ad766a6c99605701db14a620279eb26d269c
phg <paulo.grande@matera.com>
2023-12-12 10:06:12 -0300

    Crud de Estado

---
+++ backend/src/main/java/com/fantasma/backend/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.fantasma.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fantasma.backend.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long>{
+
+}
